678e0b3eb0bce978aa6b9cc45b91ff34e331f4a5
Jack Ireland <jackireland@gmail.com>
2019-02-19 12:36:17 -0500

    SPICE output now returned using Astropy coordinates

--- heliopy/spice.py
+++ heliopy/spice.py
@@ -1,12 +1,12 @@
-from heliopy import config
-import heliopy.data.helper as helper
-import heliopy.data.spice as dataspice
-
 import os
+
+from heliopy import config
+import heliopy.data.spice as dataspice
 
 import numpy as np
 import spiceypy
 import astropy.units as u
+from astropy.coordinates import SkyCoord, ICRS
 
 data_dir = config['download_dir']
 spice_dir = os.path.join(data_dir, 'spice')
@@ -15,10 +15,10 @@ _SPICE_SETUP = False
 
 
 def _setup_spice():
-    '''
+    """
     Method to download some common files that spice needs to do orbit
     calculations.
-    '''
+    """
     if not _SPICE_SETUP:
         for kernel in dataspice.generic_kernels:
             loc = dataspice.get_kernel(kernel.short_name)
@@ -38,7 +38,6 @@ def furnish(fname):
     --------
     heliopy.data.spice.get_kernel : For attempting to automatically download
                                     kernels based on spacecraft name.
-
     """
     if isinstance(fname, str):
         fname = [fname]
@@ -69,146 +68,111 @@ class Trajectory:
     --------
     furnish : for loading in local spice kernels.
     """
-    def __init__(self, target):
-        _setup_spice()
-        self._target = target
-        self._generated = False
-
-    def generate_positions(self, times, observing_body, frame):
+    def __init__(self, target, observing_body='Sun'):
         """
         Generate positions from a spice kernel.
 
         Parameters
         ----------
-        times : iterable of `datetime`
-            An iterable (e.g. a `list`) of `datetime` objects at which the
-            positions are calculated.
+        target :
         observing_body : str or int
             The observing body. Output position vectors are given relative to
             the position of this body. See
             https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/naif_ids.html
             for a list of bodies.
-        frame : str
-            The coordinate system to return the positions in. See
-            https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/frames.html
-            for a list of frames.
-        """
-        # Spice needs a funny set of times
-        fmt = '%Y %b %d, %H:%M:%S'
-        spice_times = [spiceypy.str2et(time.strftime(fmt)) for time in times]
-        # 'None' specifies no light-travel time correction
-        pos_vel, lightTimes = spiceypy.spkezr(
-            self.target, spice_times, frame, 'None', observing_body)
-        positions = np.array(pos_vel)[:, :3] * u.km
-        velocities = np.array(pos_vel)[:, 3:] * u.km / u.s
 
-        self._times = times
-        self._positions = positions
-        self._velocities = velocities
-        self._x = positions[:, 0]
-        self._y = positions[:, 1]
-        self._z = positions[:, 2]
-        self._vx = velocities[:, 0]
-        self._vy = velocities[:, 1]
-        self._vz = velocities[:, 2]
-        self._generated = True
+        """
+        _setup_spice()
+        self._target = target
         self._observing_body = observing_body
 
-    @property
-    def observing_body(self):
-        '''
-        Observing body. The position vectors are all specified relative to
-        this body.
-        '''
-        return self._observing_body
+        # SPICE frame used. The coordinate system to return the positions in. See
+        # https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/frames.html for a list of frames.
+        # This is set to J2000 as this can be used by the Astropy coordinate framework.
+        self._spice_frame = 'J2000'
 
-    @property
-    def times(self):
-        '''
-        The `list` of `datetime` at which positions were last sampled.
-        '''
-        return self._times
+        # SPICE format for times
+        self._fmt = '%Y %b %d, %H:%M:%S'
 
-    @property
-    def x(self):
-        '''
-        x coordinates of position.
-        '''
-        return self._x
-
-    @property
-    def y(self):
-        '''
-        y coordinates of position.
-        '''
-        return self._y
-
-    @property
-    def z(self):
-        '''
-        z coordinates of position.
-        '''
-        return self._z
-
-    @property
-    def r(self):
-        '''
-        Magnitude of position vectors.
-        '''
-        return np.sqrt(self.x**2 + self.y**2 + self.z**2)
-
-    @property
-    def vx(self):
+    def coordinate_and_velocity(self, time, correction="NONE"):
         """
-        x component of velocity.
-        """
-        return self._vx
-
-    @property
-    def vy(self):
-        """
-        y component of velocity.
-        """
-        return self._vy
-
-    @property
-    def vz(self):
-        """
-        z component of velocity.
-        """
-        return self._vz
-
-    @property
-    def speed(self):
-        '''
-        Speed (magnitude of velocity vectors).
-        '''
-        return np.sqrt(self.vx**2 + self.vy**2 + self.vz**2)
-
-    @property
-    def generated(self):
-        '''
-        ``True`` if positions have been generated, ``False`` otherwise.
-        '''
-        return self._generated
-
-    @property
-    def target(self):
-        '''
-        The body whose coordinates are being calculated.
-        '''
-        return self._target
-
-    def change_units(self, unit):
-        """
-        Convert the positions to different units.
+        Returns the spatial position and velocity of the target at a specific time.
 
         Parameters
         ----------
-        unit : astropy.units.Quantity
-            Must be a unit of length (e.g. km, m, AU).
+        time :
+        correction :
+
+        Returns
+        -------
+        (position, velocity) : `~tuple`
+            A two-element tuple containing position and velocity of the target. The first
+            element of the tuple is the position of the target returned as an Astropy
+            coordinate in the ICRS frame using the Cartesian representation.
+            The second element of the tuple is the three-dimensional velocity components
+            of the target velocity and is returned as three-element Astropy Quantity array.
+            These entries are the component velocities of the target in the J2000 x, y, and
+            z directions.  The velocity is quoted in kilometers per second.
         """
-        self._x = self._x.to(unit)
-        self._y = self._y.to(unit)
-        self._z = self._z.to(unit)
-        self._positions = self._positions.to(unit)
+
+        # Format the input time
+        spice_time = spiceypy.str2et(time.strftime(self._fmt))
+
+        # Calculate
+        position_and_velocity, lightTimes = spiceypy.spkezr(self._target,
+                                                            spice_time,
+                                                            self._spice_frame,
+                                                            correction,
+                                                            self._observing_body)
+
+        # Extract the position and the velocity
+        position = np.array(position_and_velocity)[0:3] * u.km
+        velocities = np.array(position_and_velocity)[:, 3:] * u.km / u.s
+
+        return SkyCoord(position[0], position[1], position[2],
+                        frame=ICRS, representation_type='cartesian', obstime=time), velocities
+
+    def coordinate(self, time, correction="NONE"):
+        """
+        Returns the spatial position of the target at a specific time.
+
+        Parameters
+        ----------
+        time :
+        correction :
+
+        Returns
+        -------
+
+        """
+        return self.coordinate_and_velocity(time, correction=correction)[0]
+
+    def velocity(self, time, correction="NONE"):
+        """
+        Returns the velocity of the target at a specific time.
+
+        Parameters
+        ----------
+        time :
+        correction :
+
+        Returns
+        -------
+
+        """
+        return self.coordinate_and_velocity(time, correction=correction)[1]
+
+    @property
+    def observing_body(self):
+        """
+        Observing body. The position vectors are all specified relative to
+        this body.
+        """
+        return self._observing_body
+
+    @property
+    def target(self):
+        """
+        The body whose coordinates are being calculated.
+        """
+        return self._target
