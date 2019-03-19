cbf8915164c8bb9e7e64e2926fe88efb97e242b8
Jack Ireland <jackireland@gmail.com>
2019-03-19 15:57:48 -0400

    added to the plots

--- examples/plot_spice_orbit.py
+++ examples/plot_spice_orbit.py
@@ -12,6 +12,7 @@ its orbit for the first year.
 import heliopy.data.spice as spicedata
 import heliopy.spice as spice
 from datetime import datetime, timedelta
+from astropy.time import Time
 import astropy.units as u
 import numpy as np
 
@@ -30,18 +31,42 @@ psp = spice.Trajectory('SPP')
 
 ###############################################################################
 # Generate a time for every day between starttime and endtime
-starttime = datetime(2018, 8, 14)
-endtime = starttime + timedelta(days=365)
+starttime = Time("2018-09-02")
+endtime = starttime + 365*u.day
 times = []
-while starttime < endtime:
-    times.append(starttime)
-    starttime += timedelta(hours=6)
-
 
 ###############################################################################
-# Generate positions
-psp.generate_positions(times, 'Sun', 'ECLIPJ2000')
-psp.change_units(u.au)
+# Generate positions, speeds and elevations.  The positions and speeds are
+# returned with units attached.  For ease of use later, the units are removed,
+# stored in a list, and then that list is converted to an Astropy Quantity
+# with the same units.  This is the recommended way of storing a Quantity
+# array.
+x_positions = []
+y_positions = []
+z_positions = []
+radial_distances = []
+speeds = []
+elevations = []
+while starttime < endtime:
+    position = psp.coordinate(starttime)
+    x_positions.append(position.x.to(u.au))
+    y_positions.append(position.y.to(u.au))
+    z_positions.append(position.z.to(u.au))
+    radial_distance = np.sqrt(np.sum(position.x**2 + position.y**2 + position.z**2)).value
+    radial_distances.append(radial_distance)
+    elevations.append(np.rad2deg(np.arcsin(position.z.value / radial_distance)))
+    speeds.append(psp.speed(starttime).value)
+    times.append(starttime)
+    starttime += 6*u.hour
+
+x_positions = x_positions * u.au
+y_positions = y_positions * u.au
+z_positions = z_positions * u.au
+radial_distances = (radial_distances * u.km).to(u.au)
+elevations = elevations * u.deg
+speeds = speeds * u.km/u.s
+times_float = [(t - times[0]).to(u.s).value for t in times] * u.s
+times = Time(times)
 
 ###############################################################################
 # Plot the orbit. The orbit is plotted in 3D
@@ -51,28 +76,32 @@ from astropy.visualization import quantity_support
 quantity_support()
 
 # Generate a set of timestamps to color the orbits by
-times_float = [(t - psp.times[0]).total_seconds() for t in psp.times]
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-kwargs = {'s': 3, 'c': times_float}
-ax.scatter(psp.x, psp.y, psp.z, **kwargs)
-ax.set_xlim(-1, 1)
-ax.set_ylim(-1, 1)
-ax.set_zlim(-1, 1)
+with quantity_support():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    kwargs = {'s': 3, 'c': times_float}
+    ax.scatter(x_positions, y_positions, z_positions, **kwargs)
+    ax.text(0, 0, 0, 'Sun', (1, 1, 0), color='k')
+    ax.plot([-1, 1], [0, 0], [0, 0])
+    ax.plot([0, 0], [-1, 1], [0, 0])
+    ax.plot([0, 0], [0, 0], [-1, 1])
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.set_zlim(-1, 1)
 
 ###############################################################################
 # Plot radial distance and elevation as a function of time
-elevation = np.rad2deg(np.arcsin(psp.z / psp.r))
 
-fig, axs = plt.subplots(3, 1, sharex=True)
-axs[0].plot(psp.times, psp.r)
-axs[0].set_ylim(0, 1.1)
-axs[0].set_ylabel('r (AU)')
+with quantity_support():
+    fig, axs = plt.subplots(3, 1, sharex=True)
+    axs[0].plot(times.to_datetime(), radial_distances)
+    axs[0].set_ylim(0, 1.1)
+    axs[0].set_ylabel('r (AU)')
 
-axs[1].plot(psp.times, elevation)
-axs[1].set_ylabel('Elevation (deg)')
+    axs[1].plot(times.to_datetime(), elevations)
+    axs[1].set_ylabel('Elevation (deg)')
 
-axs[2].plot(psp.times, psp.speed)
-axs[2].set_ylabel('Speed (km/s)')
+    axs[2].plot(times.to_datetime(), speeds)
+    axs[2].set_ylabel('Speed (km/s)')
 
 plt.show()
